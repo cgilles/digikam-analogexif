9df6464ad0f483073e4ef98c1ff4f580287f8b5d
kaeurialias <kaeurialias@7c1462d6-8601-4bb3-828b-300c8d2eb047>
2010-04-24 20:08:24 +0000

    Deleting MetaCategories table

--- exiftreemodel.cpp
+++ exiftreemodel.cpp
@@ -1,17 +1,35 @@
 #include "exiftreemodel.h"
+#include "exifutils.h"
 
-ExifTreeModel::ExifTreeModel(QObject *parent) : QAbstractItemModel(parent)
+#include <QSqlQuery>
+#include <QStringList>
+#include <QVariantList>
+#include <QFont>
+#include <QBrush>
+
+#include <cmath>
+
+ExifTreeModel::ExifTreeModel(QSqlDatabase& db, QObject *parent) : QAbstractItemModel(parent), dataBase(db)
 {
 	// create empty root item
 	rootItem = new ExifItem("", "", QVariant());
 
-	db = QSqlDatabase::addDatabase("QSQLITE", ":/analogexif.db");
-	db.open();
+	// create and read the values of the used metatags
+	populateModel();
+	editable = false;
 }
 
 ExifTreeModel::~ExifTreeModel()
 {
 	delete rootItem;
+}
+
+// clear data
+void ExifTreeModel::clear()
+{
+	reset();
+	editable = false;
+	rootItem->reset();
 }
 
 // open file and read metadata
@@ -34,13 +52,14 @@ bool ExifTreeModel::openFile(QString filename)
 		return false;
 	}
 
-	// clear all
-	rootItem->removeChildren();
+	// read all tags from model
+	if(readMetaValues())
+	{
+		editable = true;
+		return true;
+	}
 
-	// create and read the values of the used metatags
-	populateModel();
-
-	return true;
+	return false;
 }
 
 // get item by its index
@@ -74,7 +93,10 @@ Qt::ItemFlags ExifTreeModel::flags(const QModelIndex &index) const
 	if(index.column() == 0)
 		return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 
-	return Qt::ItemIsEditable | Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+	if(editable)
+		return Qt::ItemIsEditable | Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+	else
+		return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
 
 // create index for the given position in the tree
@@ -115,7 +137,7 @@ QVariant ExifTreeModel::data(const QModelIndex &index, int role) const
 		return QVariant();
 
 	// return only for Display and Edit role
-	if (role != Qt::DisplayRole && role != Qt::EditRole)
+	if (role != Qt::DisplayRole && role != Qt::EditRole && role != GetTypeRole && role != Qt::FontRole)
 		return QVariant();
 
 	ExifItem *item = getItem(index);
@@ -123,16 +145,93 @@ QVariant ExifTreeModel::data(const QModelIndex &index, int role) const
 	// for caption and column 0 - return caption
 	if(item->isCaption())
 	{
-		if(index.column() == 0)
+		if((index.column() == 0) && (role == Qt::DisplayRole))
 			return item->value();
 		else
 			return QVariant();
 	}
 
+	if((index.column() == 1) && (role == Qt::FontRole) && item->isDirty())
+	{
+		QFont f;
+		f.setBold(true);
+
+		return f;
+	}
+
 	// for meta data return tag name for column 0 and value for column 1
-	if(index.column() == 0)
+	if((index.column() == 0)  && (role == Qt::DisplayRole))
 		return item->tagText();
 
+	// return tag type
+	if(role == GetTypeRole)
+		return item->tagType();
+
+	if(item->value() == QVariant())
+		return QVariant();
+
+	// return value according to the tag type
+	switch(item->tagType())
+	{
+	case ExifItem::TagString:
+		if(role == Qt::DisplayRole)
+			return QString(item->format()).arg(item->value().toString());
+		else if(role == Qt::EditRole)
+			return item->value();
+		break;
+	case ExifItem::TagInteger:
+	case ExifItem::TagUInteger:
+	case ExifItem::TagISO:
+		if(role == Qt::DisplayRole)
+			return QString(item->format()).arg(item->value().toInt());
+		else if(role == Qt::EditRole)
+			return item->value();
+		break;
+	case ExifItem::TagRational:
+	case ExifItem::TagURational:
+	case ExifItem::TagAperture:
+	case ExifItem::TagApertureAPEX:
+		{
+			QVariantList rational = item->value().toList();
+			double value = (double)rational.at(0).toInt() / (double)rational.at(1).toInt();
+
+			// adjust APEX values
+			if(item->tagType() == ExifItem::TagApertureAPEX)
+				value = exp(value*log(2.0)*0.5);
+
+			if(role == Qt::DisplayRole)
+				return QString(item->format()).arg(value, 0, 'f', 1);
+			else if(role == Qt::EditRole)
+				return value;
+			break;
+		}
+	case ExifItem::TagFraction:
+	case ExifItem::TagShutter:
+		{
+			QVariantList rational = item->value().toList();
+			double value = (double)rational.at(0).toInt() / (double)rational.at(1).toInt();
+
+			if(role == Qt::DisplayRole)
+			{
+				if((value >= 0.5) && (item->tagType() == ExifItem::TagShutter))
+				{
+					return QString(item->format()).arg(value);
+				}
+				else
+				{
+					return QString(item->format()).arg(QString("%1/%2").arg(rational.at(0).toInt()).arg(rational.at(1).toInt()));
+				}
+			}
+			else if(role == Qt::EditRole)
+				return rational;
+
+			break;
+		}
+	default:
+		break;
+	}
+
+	// just return item value
 	return item->value();
 }
 
@@ -143,7 +242,72 @@ bool ExifTreeModel::setData(const QModelIndex &index, const QVariant &value, int
 		return false;
 
 	ExifItem *item = getItem(index);
-	item->setValue(value);
+
+	// don't change the same values
+	if(item->value() == value)
+		return false;
+
+	// return value according to the tag type
+	switch(item->tagType())
+	{
+	case ExifItem::TagString:
+		item->setValue(value.toString(), true);
+		break;
+	case ExifItem::TagInteger:
+	case ExifItem::TagUInteger:
+	case ExifItem::TagISO:
+		item->setValue(value.toInt(), true);
+		break;
+	case ExifItem::TagRational:
+	case ExifItem::TagURational:
+	case ExifItem::TagAperture:
+	case ExifItem::TagApertureAPEX:
+		{
+			QVariantList rational;
+			double val = value.toDouble();
+
+			// correction for APEX values
+			if(item->tagType() == ExifItem::TagApertureAPEX)
+			{
+				val = 2*log(val)/log(2.0);
+			}
+
+			rational = item->value().toList();
+			if(rational != QVariantList())
+			{
+				// special check whether value is changed for rational numbers
+				double oldValue = (double)rational.at(0).toInt() / (double)rational.at(1).toInt();
+
+				// compare APEX values up to the second digit after the decimal point
+				if(item->tagType() == ExifItem::TagApertureAPEX)
+				{
+					if((int)(oldValue*100) == (int)(val*100))
+						return false;
+				}
+				else
+				{
+					if(oldValue == val)
+						return false;
+				}
+			}
+			rational.clear();
+
+			int first, second;
+			ExifUtils::doubleToFraction(val, &first, &second);
+
+			rational << first << second;
+
+			item->setValue(rational, true);
+			break;
+		}
+	case ExifItem::TagFraction:
+	case ExifItem::TagShutter:
+		item->setValue(value.toList(), true);
+		break;
+	default:
+		return false;
+		break;
+	}
 
 	emit dataChanged(index, index);
 
@@ -153,19 +317,135 @@ bool ExifTreeModel::setData(const QModelIndex &index, const QVariant &value, int
 // populate model
 void ExifTreeModel::populateModel()
 {
-
 	// connect to internal database
-//	QSqlQuery query = new QSqlQuery("SELECT 
+	// QSqlQuery query("SELECT b.id, b.Description, a.TagName, a.TagText, a.PrintFormat, a.TagType FROM MetaTags a, MetaCategories b where a.CategoryId=b.id ORDER BY b.OrderBy, a.OrderBy");
+	QSqlQuery query("SELECT a.GearType, b.TagName, b.TagText, b.PrintFormat, b.TagType FROM GearTemplate a, MetaTags b WHERE b.id=a.TagId ORDER BY a.GearType, a.OrderBy, b.OrderBy");
 
-	beginInsertRows(QModelIndex(), 0, ModelItemsMax);
-	ExifItem* headerItem = rootItem->insertChild("", "", tr("Camera description"));
+	int curCategoryId = -1, nRows = 0;
+	ExifItem* headerItem = NULL;
 
+	while(query.next())
+	{
+		if(query.value(0).toInt() != curCategoryId)
+		{
+			// we have a new category - create a head element
+			curCategoryId = query.value(0).toInt();
 
+			QString categoryTitle;
 
-	headerItem->insertChild("Exif.Maker", "Maker", "Canon");
-	headerItem->insertChild("Exif.Model", "Model", "Canon EOS 30");
+			switch(curCategoryId)
+			{
+			case 0:
+				categoryTitle = tr("Camera");
+				break;
+			case 1:
+				categoryTitle = tr("Lens");
+				break;
+			case 2:
+			case 3:
+				categoryTitle = tr("Frame");
+				break;
+			}
 
-	headerItem = rootItem->insertChild("", "", "Lens");
-	headerItem->insertChild("Exif.Model", "Model", "Canon EF 50mm f/1.8 II");
+			headerItem = rootItem->insertChild("", "", curCategoryId);
+			nRows++;
+			
+		}
+
+		// insert a tag
+		headerItem->insertChild(query.value(2).toString(), query.value(3).toString(), QVariant(), query.value(4).toString(), query.value(5).toInt());
+		nRows++;
+	}
+	beginInsertRows(QModelIndex(), 0, nRows);
 	endInsertRows();
+}
+
+bool ExifTreeModel::readMetaValues()
+{
+	Exiv2::ExifData &exifData = exifHandle->exifData();
+
+	// browse through all categories
+	for(int i = 0; i < rootItem->childCount(); i++)
+	{
+		ExifItem* category = rootItem->child(i);
+		for(int j = 0; j < category->childCount(); j++)
+		{
+			ExifItem* tag = category->child(j);
+
+			try
+			{
+				QString tagType = tag->tagName().split(".").at(0);
+				if(tagType == "Exif")
+				{
+					// Exif data
+					Exiv2::Exifdatum tagValue = exifData[tag->tagName().toStdString()];
+
+					Exiv2::TypeId typId = tagValue.typeId();
+					switch(typId)
+					{
+					case Exiv2::asciiString:
+					case Exiv2::comment:
+						tag->setValue(QString::fromStdString(exifData[tag->tagName().toStdString()].toString()));
+						break;
+					case Exiv2::unsignedByte:
+					case Exiv2::unsignedShort:
+					case Exiv2::unsignedLong:
+					case Exiv2::signedByte:
+					case Exiv2::signedShort:
+					case Exiv2::signedLong:
+						tag->setValue(exifData[tag->tagName().toStdString()].toLong());
+						break;
+					case Exiv2::unsignedRational:
+					case Exiv2::signedRational:
+						{
+							Exiv2::Rational val = exifData[tag->tagName().toStdString()].toRational();
+							QVariantList rational;
+							rational << val.first << val.second;
+							tag->setValue(rational);
+							break;
+						}
+					default:
+						tag->setValue(QVariant());
+						break;
+					}
+				}
+				else if(tagType == "Iptc")
+				{
+					// IPTC tags
+					tag->setValue(QVariant());
+				}
+				else if(tagType == "Xmp")
+				{
+					// XMP tags
+					tag->setValue(QVariant());
+				}
+			}
+			catch(Exiv2::AnyError&)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+void ExifTreeModel::reload()
+ {
+	 reset();
+
+	 if(readMetaValues())
+		editable = true;
+ }
+
+void ExifTreeModel::setValues(QVariantList& values)
+{
+	if(values.count() < 2)
+		return;
+
+	for(int i = 0; i < values.count(); i += 2)
+	{
+		if(rootItem->findSetTagValueFromString(values.takeAt(i).toString(), values.takeAt(i+1).toString(), true))
+			emit dataChanged(QModelIndex(), QModelIndex());
+	}
 }
