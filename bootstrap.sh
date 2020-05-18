216cae8b3821497af154326dbd2d812c2cf2f967
Gilles Caulier <caulier.gilles@gmail.com>
2020-05-18 12:08:43 +0200

    add plugin files and cmake scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# Copyright (c) 2008-2020 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+# Copyright (c) 2008-2019, Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
-# Copy this script on root folder where are source code
 
-#export VERBOSE=1
-
-# We will work on command line using MinGW compiler
-export MAKEFILES_TYPE='Unix Makefiles'
-
-mkdir -p build
+rm -rf build
+mkdir build
 cd build
 
-qmake ..
-
-make
+cmake . \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -G"Unix Makefiles" \
+      -Wno-dev \
+      ..
